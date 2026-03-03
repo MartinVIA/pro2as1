a5c98775deca1d02d3cbd6c82a39c19d1decfd14
HNande <78953559+HNande@users.noreply.github.com>
2026-03-04 00:40:32 +0100

    VinylObserver extends PropertyChangeListener

--- root/src/utils/PropertyChangeSubject.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-import java.beans.PropertyChangeListener;
-
-public interface PropertyChangeSubject {
-  void  addPropertyChangeListener(PropertyChangeListener listener);
-  void removePropertyChangeListener(PropertyChangeListener listener);
-}
