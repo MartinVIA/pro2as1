99efda4c25b2f22b2d79ec26e3e7fc816fc0ca85
axilleaskampas5 <362344@via.dk>
2026-03-17 11:11:48 +0100

    Nwe vinyls

--- src/model/VinylLibraryManager.java
+++ src/model/VinylLibraryManager.java
@@ -10,6 +10,14 @@ public class VinylLibraryManager implements VinylModel {
     vinylList = FXCollections.observableArrayList();
     addVinyl("Hamburger","Biggie Cheese", 2026);
     addVinyl("Buvany Special"," GMAN productions", 6767);
+    addVinyl("Nandinho Brazil", "Nande",2026);
+    addVinyl("Scrum master", "Guillermo", 2005);
+    addVinyl("18", "Moby",2010);
+    addVinyl("Pizza","Bob", 2007);
+    addVinyl("Buvan Jr"," GMAN productions", 1980);
+    addVinyl("Play", "Moby",2007);
+    addVinyl("Product Owner", "Buvan", 2000);
+    addVinyl("Achilleas", "Kampas",2010);
   }
   public void addVinyl(String title,String artist,int releaseYear) {
     Vinyl vinyl = new Vinyl(title,artist,releaseYear);
