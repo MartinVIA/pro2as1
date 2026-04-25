beb3dabd4e9346b4d88409946d3183520f02f067
HNande <78953559+HNande@users.noreply.github.com>
2026-04-25 16:34:55 +0200

    Socket attempt 2

--- src/server/VinylServer.java
+++ src/server/VinylServer.java
@@ -2,19 +2,28 @@ package server;
 
 import javafx.collections.ObservableList;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.ServerSocket;
-import javafx.collections.ObservableList;
-
-import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class VinylServer {
   public static void main(String[] args) throws IOException {
+    final int PORT = 4026;
 
-    ServerSocket serverSocket = new ServerSocket(5678);
+    ServerSocket serverSocket = new ServerSocket(PORT);
+    while (true){
     System.out.println("Waiting for connection on " + serverSocket.getLocalPort());
-
+    Socket socket = serverSocket.accept();
+    BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
+    String request = input.readLine();
+      System.out.println("Client "+ request);
+      String reply = request.toLowerCase();
+      System.out.println("Server "+ reply);
+      output.println(reply);
+    }
   }
 }
